ba624bf81b3ed2dd6964c2b9ac76cac5eaaa647f
fedetrifo06 <61948220+fedetrifo06@users.noreply.github.com>
2022-01-09 18:06:54 +0100

    Updated to 1.18.1

--- src/main/java/mod/omoflop/customgui/mixin/RenderSystemMixin.java
+++ src/main/java/mod/omoflop/customgui/mixin/RenderSystemMixin.java
@@ -7,30 +7,37 @@ import net.fabricmc.api.Environment;
 import net.minecraft.util.Identifier;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Group;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-import static com.mojang.blaze3d.systems.RenderSystem._setShaderTexture;
-import static com.mojang.blaze3d.systems.RenderSystem.recordRenderCall;
-
-@Mixin(value = RenderSystem.class, remap = false)
+@Mixin({RenderSystem.class})
 @Environment(EnvType.CLIENT)
 public abstract class RenderSystemMixin {
-
-    @Inject(method = "setShaderTexture(ILnet/minecraft/util/Identifier;)V", at = @At("HEAD"), cancellable = true)
-    private static void setShaderTextureMixin(int i, Identifier id, CallbackInfo ci) {
-        
-        if (!id.getPath().contains("textures/gui")) return;
-        Identifier newID = OverrideManager.getOverride(id);
-
-        if (newID != null) {
-            final Identifier finalID = newID;
-            if (!RenderSystem.isOnRenderThread()) {
-                recordRenderCall(() -> _setShaderTexture(i, finalID));
-            } else {
-                _setShaderTexture(i, finalID);
-            }
-            ci.cancel();
-        }
-    }
+  @Inject(method = {"setShaderTexture(ILnet/minecraft/util/Identifier;)V"}, at = {@At("HEAD")}, cancellable = true, remap = false)
+  @Group(name = "setShaderMixin")
+  private static void setShaderTextureDev(int i, Identifier id, CallbackInfo ci) {
+    setShaderTextureMixin(i, id, ci);
+  }
+  
+  @Inject(method = {"setShaderTexture(ILnet/minecraft/class_2960;)V"}, at = {@At("HEAD")}, cancellable = true, remap = false)
+  @Group(name = "setShaderMixin")
+  private static void setShaderTextureProd(int i, Identifier id, CallbackInfo ci) {
+    setShaderTextureMixin(i, id, ci);
+  }
+  
+  private static void setShaderTextureMixin(int i, Identifier id, CallbackInfo ci) {
+    if (!id.getPath().contains("textures/gui"))
+      return; 
+    Identifier newID = OverrideManager.getOverride(id);
+    if (newID != null) {
+      Identifier finalID = newID;
+      if (!RenderSystem.isOnRenderThread()) {
+        RenderSystem.recordRenderCall(() -> RenderSystem._setShaderTexture(i, finalID));
+      } else {
+        RenderSystem._setShaderTexture(i, finalID);
+      } 
+      ci.cancel();
+    } 
+  }
 }
